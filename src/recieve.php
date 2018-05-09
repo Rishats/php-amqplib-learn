c47ddc0e8232901970a49b4b6f47c16a4a19dfae
Rishats <sultanov-rishat@inbox.ru>
2018-05-09 20:58:01 +0600

    first commit

---
+++ src/recieve.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once '../vendor/autoload.php';
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+$host = '192.168.8.99';
+$port = 8080;
+$user = 'guest';
+$pass = 'guest';
+
+$connection = new AMQPStreamConnection($host, $port, $user, $pass);
+$channel = $connection->channel();
+
+$channel->queue_declare('hello', false, false, false, false);
+
+$msg = new AMQPMessage('Hello World!');
+$channel->basic_publish($msg, '', 'hello');
+
+echo " [x] Sent 'Hello World!'\n";
+
+$channel->close();
+$connection->close();
